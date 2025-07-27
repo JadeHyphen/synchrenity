abe8ee69df70e58a70af19d80ce736728c8b6bd5
Jade Yurei Lewis <jadebyurei@Jades-MacBook-Air.local>
2025-07-27 11:41:13 -0400

    Fix PHP tag and structure in ForgePaginationRuntimeTest.php

--- tests/ForgePaginationRuntimeTest.php
+++ tests/ForgePaginationRuntimeTest.php
@@ -1,30 +1,23 @@
 <?php
 namespace Tests;
 use PHPUnit\Framework\TestCase;
-
-class ForgePaginationRuntimeTest extends TestCase
-{
-    public function testPaginationRenders()
-    {
-        $this->assertTrue(true, 'Forge pagination renders HTML.');
-    }
-}
-<?php
 use Synchrenity\Pagination\SynchrenityPaginator;
 use Synchrenity\Forge\Components\ForgePagination;
 
 require_once __DIR__ . '/../lib/Pagination/SynchrenityPaginator.php';
 require_once __DIR__ . '/../lib/Forge/Components/ForgePagination.php';
 
-// Sample data
-$data = range(1, 100);
-$page = 2;
-$perPage = 10;
-$route = '/test-pagination';
-$params = ['foo' => 'bar'];
-
-$paginator = new SynchrenityPaginator($data, count($data), $page, $perPage, $route, $params);
-
-// Render pagination HTML
-$html = ForgePagination::render($paginator, ['seo' => true]);
-echo $html;
+class ForgePaginationRuntimeTest extends TestCase
+{
+    public function testPaginationRendersHtml()
+    {
+        $data = range(1, 100);
+        $page = 2;
+        $perPage = 10;
+        $route = '/test-pagination';
+        $params = ['foo' => 'bar'];
+        $paginator = new SynchrenityPaginator($data, count($data), $page, $perPage, $route, $params);
+        $html = ForgePagination::render($paginator, ['seo' => true]);
+        $this->assertStringContainsString('pagination-nav', $html, 'Pagination HTML should contain navigation');
+    }
+}
